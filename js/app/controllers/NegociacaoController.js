63a5d91920afc54bb655d2c525db55045ed97f1a
Paulo Tiago <paulotiagocm@hotmail.com>
2021-01-30 07:49:39 -0300

    feature: atualizando view interceptando model

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,9 +4,25 @@ class NegociacaoController {
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        const self = this;
 
         this._negociacoesView = new NegociacoesView(document.querySelector('#negociacoesTable'));
-        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, reciever) {
+                if (
+                    ['adiciona','esvazia'].includes(prop) &&
+                    typeof(target[prop]) === typeof(Function)
+                ) {
+                    return function() {
+                        console.log(`interceptando ${prop}`)
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, reciever);
+            }
+        });
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView(document.querySelector('#mensagemView'));
@@ -21,7 +37,6 @@ class NegociacaoController {
         );
         this._listaNegociacoes.adiciona(negociacao);
 
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = "Negociação criada com sucesso";
         this._mensagemView.update(this._mensagem);
     }
