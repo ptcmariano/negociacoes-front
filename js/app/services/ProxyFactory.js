fb5234fbd797afb5b84bddc0959763360a0ad455
Paulo Tiago <paulotiagocm@hotmail.com>
2021-02-05 00:11:30 -0300

    refactor: legibilidade das validacoes

--- js/app/services/ProxyFactory.js
+++ js/app/services/ProxyFactory.js
@@ -1,11 +1,11 @@
 
-class ProxyFactory{
+class ProxyFactory {
     static create(objeto, props, acao) {
         return new Proxy(objeto, {
             get(target, prop, reciever) {
                 if (
-                    props.includes(prop) &&
-                    typeof(target[prop]) === typeof(Function)
+                    ProxyFactory._inLista(props, prop) &&
+                    ProxyFactory._isFuncao(target[prop])
                 ) {
                     return function() {
                         Reflect.apply(target[prop], target, arguments);
@@ -15,12 +15,18 @@ class ProxyFactory{
                 return Reflect.get(target, prop, reciever);
             },
             set(target, prop, value, reciever) {
-                if (props.includes(prop)) {
+                if (ProxyFactory._inLista(props, prop)) {
                     console.log(`vai setar`, prop);
                     acao(target);
                 }
                 return Reflect.set(target, prop, value, reciever);
             }
         });
+    }
+    static _isFuncao(funcao) {
+        return typeof(funcao) === typeof(Function);
+    }
+    static _inLista(props, prop) {
+        return props.includes(prop);
     }
 }
